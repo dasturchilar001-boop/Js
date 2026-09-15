e8553b14ae4522f18e84b652bc0880423a7efde5
dasturchilar001-boop <Dasturchilar001@gmail.com>
2026-09-15 10:41:01 +0500

    Js home

---
+++ index.js
@@ -0,0 +1,58 @@
+
+
+
+
+
+let Otam = prompt("Otangizni yoshi nechida");
+console.log(Otam);
+let OtamName = prompt("Otangizni Ismi nima?");
+console.log(OtamName);
+let OtamSurname = prompt("Otangizni familyasi nima ");
+console.log(OtamSurname);
+let OtamIsh = prompt("Otangizni Ish joyi Qayerda")
+console.log(OtamIsh);
+
+alert("Salom mening otamni " + Otam + " yoshlari " + OtamName + "da" + OtamIsh + " Ishlaydilar" + OtamSurname +"Otamni familyasi ");
+
+
+
+let Onam = prompt("Onangizni yoshi nechida");
+console.log(Onam);
+let OnamName = prompt("Onangizni Ismi nima?");
+console.log(OnamName);
+let OnamSurname = prompt("Onangizni familyasi nima ");
+console.log(OnamSurname);
+let OnamIsh = prompt("Onangizni Ish joyi Qayerda")
+console.log(OnamIsh);
+
+alert("Salom mening otamni " + Onam + " yoshlari " + OnamName + "da" + OnamIsh + " Ishlaydilar" + OnamSurname +"Onamni familyasi ");
+
+
+
+let Singlim = prompt("Singlingizni yoshi nechida");
+console.log(Singlim);
+let SinglimName = prompt("Singlingizni Ismi nima?");
+console.log(SinglimName);
+let SinglimSurname = prompt(" Singlingizni familyasi nima ");
+console.log(SinglimSurname);
+let SinglimIsh = prompt("Singlingizni Ish joyi Qayerda")
+console.log(SinglimIsh);
+
+alert("Salom mening Singlimni " + Singlim + " yoshi " + SinglimName + "da" + SinglimIsh + " Ishlaydi" + SinglimSurname +"Singlimni familyasi ");
+
+
+
+
+
+
+
+let Singlimm = prompt("Singlingizni yoshi nechida");
+console.log(Singlimm);
+let SinglimmName = prompt("Singlingizni Ismi nima?");
+console.log(SinglimmName);
+let SinglimmSurname = prompt(" Singlingizni familyasi nima ");
+console.log(SinglimmSurname);
+let SinglimmIsh = prompt("Singlingizni Ish joyi Qayerda")
+console.log(SinglimmIsh);
+
+alert("Salom mening Singlimni " + Singlimm + " yoshi " + SinglimmName + "da" + SinglimmIsh + " Ishlaydi" + SinglimmSurname +"Singlimni familyasi ");
